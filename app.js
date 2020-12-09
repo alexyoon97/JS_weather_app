4192e36a7e20577d134a79c7d515d01f0ff2f0ca
Alex Yoon <wonsangalex@gmail.com>
2020-12-09 01:19:40 -0800

    split top and bottom

--- app.js
+++ app.js
@@ -11,13 +11,14 @@ var top_icon_img_urls = [];
 var bot_icon_img_urls = [];
 
 var NounProject = require('the-noun-project'),
-nounProject = new NounProject({
+    nounProject = new NounProject({
     key: 'd37f53a4717646c290b616d1499e7988',
     secret: '56779a466caf4070b58998afc64df940'
 });
-app.post('/api', async(request,response) => {
-    await FindIcons(request);
-    console.log(top_icon_img_urls);
+
+app.use('/api',FindIcons);
+
+app.post('/api', (request,response) => {
     response.json({
         status: 'success',
         top_icon_img: top_icon_img_urls,
@@ -27,24 +28,44 @@ app.post('/api', async(request,response) => {
     bot_icon_img_urls = [];
 
 })
-function FindIcons(request){
-    for(var i in request.body){
-        for(var j in request.body[i]){
-            console.log(request.body[i][j]);
-            nounProject.getIconsByTerm(request.body[i][j], function (err, data) {
-                if (!err) {
-                    top_icon_img_urls.push(data.icons[1].preview_url);
-
-                    // if(count == 0){
-                    //     console.log(i)
-                    // }else{
-                    //     bot_icon_img_urls.push(data.icons[0].preview_url);
-                    // }
-                }
-                else{
-                    console.log(err);
-                }
-             });
+function FindIcons(response,request,next){
+    for(var i in response.body){
+        for(var j in response.body[i]){
+            if(i.toString().includes("bot",0)){
+                nounProject.getIconsByTerm(response.body[i][j], function (err, data) {
+                    if (!err) {
+                        bot_icon_img_urls.push(data.icons[2].preview_url);
+                    }
+                    else{
+                        console.log(err + response.body);
+                    }
+                 });
+            }
+            else{
+                nounProject.getIconsByTerm(response.body[i][j], function (err, data) {
+                    if (!err) {
+                        top_icon_img_urls.push(data.icons[2].preview_url);
+                    }
+                    else{
+                        console.log(err + response.body);
+                    }
+                 });
+            }
+            
         }
     }
+    console.log(bot_icon_img_urls);
+    next();
 }
+// function fetchURL(outfit_type){
+//     var url;
+//     nounProject.getIconsByTerm(outfit_type, function (err, data) {
+//         if (!err) {
+//             url = data.icons[0].attribution_preview_url;
+//             return url;
+//         }
+//         else{
+//             console.log(err);
+//         }
+//      });
+// }
